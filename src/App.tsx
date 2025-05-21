beea9c3444b6ff44aa0cf4547b790348766f470f
Giovanni <gmarques@kehops.com>
2025-05-21 14:54:24 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <a href="https://react.dev" target="_blank">
+        test
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
     </>

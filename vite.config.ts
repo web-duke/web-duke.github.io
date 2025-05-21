c326d890696c6dc12d5921b451fc975f8dd59f04
Giovanni <gmarques@kehops.com>
2025-05-21 15:07:17 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    outDir: "docs",
+    assetsDir: "assets",
+  },
   plugins: [react()],
 });

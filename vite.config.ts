dac217e2e3430f00b1bf17043289a99c142a4b1d
Giovanni <gmarques@kehops.com>
2025-05-21 16:58:13 -0400

    common vars scss file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +9,11 @@ export default defineConfig({
     assetsDir: "assets",
   },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "/src/styles/vars" as *;\n`,
+      },
+    },
+  },
 });

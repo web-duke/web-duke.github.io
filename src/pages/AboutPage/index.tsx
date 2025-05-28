78a9922d56a08bfdc77f33718f10931e077b71b7
Giovanni <gmarques@kehops.com>
2025-05-28 13:57:46 -0400

    about

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -5,24 +5,51 @@ export const AboutPage = () => {
   const { t } = useTranslation("translation", { keyPrefix: "pages.AboutPage" });
 
   return (
-    <div>
-      {t("intro")}
-      <a
-        href="https://www.linkedin.com/in/giovanni-mrqs/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
+    <>
+      <h1>{t("title")}</h1>
 
-      <a
-        href="https://github.com/web-duke/web-duke.github.io"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
-    </div>
+      <p>{t("txt1")}</p>
+
+      <p>{t("txt2")}</p>
+
+      <p>
+        {t("txt3")}
+        <a
+          href="https://github.com/web-duke/web-duke.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+
+      <p>
+        {t("txt4")}
+        <a
+          href="https://codepen.io/webduke/pens/popular"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Codepen
+        </a>
+        .
+      </p>
+
+      <p>
+        {t("txt5")}
+        <a
+          href="https://www.linkedin.com/in/giovanni-mrqs/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        {t("txt6")}
+        <br />
+        {t("txt7")}
+      </p>
+    </>
   );
 };
 
